b247ba1a9b4065b76a6cb412815953adf8dfe9d3
Wagnner Leivas <44724645+wagnnercsl@users.noreply.github.com>
2020-10-22 10:22:43 -0300

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,19 @@
 @section('content')
 <div class="container">
     <img src="/img/meeting.svg" alt="">
-    <h2 style="overflow-y: wrap;"></h2>
+    <h2>Controle de chamados</h2>
+    <h3 style="overflow-y: wrap;">
+        O sistema de chamados vai facilitar seu controle sobre as demandas de telemarketing.
+        
+        
+    </h3>
+    <p style="margin-top: 20px;">Entre as funcionalidades presentes estão:
+    </p>
+    <ul>
+        <li>Cadastro de usuários com diferentes permissões no sistema</li>
+        <li>
+            Exibição do estado atual dos chamados do cliente, com a função de comentários para um melhor acompanhamento
+        </li>
+    </ul>
 </div>
 @endsection
